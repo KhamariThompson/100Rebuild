9fe5fb0145fd52bf19d4fccc26e0ba67e22aa460
KhamariThompson <khamari11@gmail.com>
2025-04-30 09:31:41 -0400

    Add initial Xcode project

---
+++ 100DaysRebuild/100DaysRebuild/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  100DaysRebuild
+//
+//  Created by Khamari T on 4/30/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
